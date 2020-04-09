578f79c4ef5a7e23a1d8d9327a2562bca3d51ccd
Andrea Burattin <delas@users.noreply.github.com>
2020-04-09 13:26:38 +0200

    Update campusNetImporter.js

--- bookmarklet/campusNetImporter.js
+++ bookmarklet/campusNetImporter.js
@@ -1,3 +1,3 @@
 var path = $(location).attr('href');
 var assignmentUid = path.substring(path.indexOf("$") - 3, path.indexOf("?"));
-$.get("https://dtu.onlineeksamen.dk/api/AssignmentAdministration/GetAdditionalData/" + assignmentUid, {"headers":{"x-currentusertoken":userToken }}, function(data){ $("#container").prepend("<textarea style='position: absolute; top: 0; left: 0; width: 100%; height: 100%; z-index: 10000'>" + JSON.stringify(data) + "</textarea>"); });
+$.get("https://dtu.onlineeksamen.dk/api/AssignmentAdministration/GetHandIns/" + assignmentUid, {"headers":{"x-currentusertoken":userToken }}, function(data){ $("#container").prepend("<textarea style='position: absolute; top: 0; left: 0; width: 100%; height: 100%; z-index: 10000'>" + JSON.stringify(data) + "</textarea>"); });
