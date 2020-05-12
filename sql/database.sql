0bfbfd7cc97ee1a79a028a0312b03d3138d3beba
Andrea Burattin <delas@users.noreply.github.com>
2020-05-12 20:14:34 +0200

    Update database.sql

--- sql/database.sql
+++ sql/database.sql
@@ -16,19 +16,20 @@ CREATE TABLE IF NOT EXISTS `exams` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 
 -- Data exporting was unselected.
 -- Dumping structure for table feedback_giver.groups
 CREATE TABLE IF NOT EXISTS `groups` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `exam_id` int(10) unsigned NOT NULL DEFAULT '0',
+  `name` varchar(255) DEFAULT NULL,
   `tentative_grade` varchar(255) DEFAULT NULL,
   `feedback` text,
   PRIMARY KEY (`id`),
   KEY `FK_groups_exams` (`exam_id`),
   CONSTRAINT `FK_groups_exams` FOREIGN KEY (`exam_id`) REFERENCES `exams` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=61 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8;
 
 -- Data exporting was unselected.
 -- Dumping structure for table feedback_giver.groups_students
@@ -43,7 +44,7 @@ CREATE TABLE IF NOT EXISTS `groups_students` (
   KEY `FK_groups_students_students` (`student_id`),
   CONSTRAINT `FK_groups_students_groups` FOREIGN KEY (`group_id`) REFERENCES `groups` (`id`) ON DELETE CASCADE,
   CONSTRAINT `FK_groups_students_students` FOREIGN KEY (`student_id`) REFERENCES `students` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=301 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=109 DEFAULT CHARSET=utf8;
 
 -- Data exporting was unselected.
 -- Dumping structure for table feedback_giver.students
@@ -54,7 +55,7 @@ CREATE TABLE IF NOT EXISTS `students` (
   `img` varchar(1024) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `matricle` (`number`)
-) ENGINE=InnoDB AUTO_INCREMENT=101 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=266 DEFAULT CHARSET=utf8;
 
 -- Data exporting was unselected.
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
